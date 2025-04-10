eded37be16ab0499bb58fa6096056e21398dfb3d
brambleyq <quentinlbrambley@gmail.com>
2025-04-10 10:53:14 -0400

    readers

---
+++ npi_reader.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+
+def read(path: str) -> pd.DataFrame:
+    """Read in NPI data and rename columns"""
+
+    df = pd.read_csv(path)
+    mapper = {
+            'NPI': 'npi',
+            'Healthcare Provider Taxonomy Code_1': 'taxonomy_code',
+            'Provider Last Name (Legal Name)': 'surname',
+            'Provider First Name': 'forename',
+            'Provider First Line Business Practice Location Address': 'address',
+            'Certification Date': 'cert_date',
+            'Provider Business Practice Location Address State Name': 'city',
+            'Provider Business Practice Location Address State Name': 'state',
+            'Provider Business Practice Location Address Country Code (If outside U.S.)': 'country'
+        }
+    
+    df = df.rename(columns=mapper)[mapper.values()]
+    df.dropna(subset=['forename','surname','address'],inplace=True)
+    return df
+
+    #There are 850 missing values for taxonomy_code, address, state, and country
+    #There are 6,259 missing last names and 6,262 missing first names
+    #There are 1,505 missing values for cert_date
+
+
+if __name__ == '__main__':
+    df = read("data/npidata_pfile_20250303-20250309.csv")
+    print(df.head())
